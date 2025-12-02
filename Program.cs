dcdd6d53a9eea66b059602ed080605130dbe9796
PerLund <per@lundkvist.com>
2025-12-02 06:53:39 +0100

    Added Else results

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿
+using AdventOfCode10;
+
+if (args.Any())
+{
+    //ElseResults.Create(@"C:\Projekt\AdventOfCode3\Input\Else.json");
+    var json = args.First() == "Web" ? ElseResults.GetTopList(args[1]) : File.ReadAllText(args.First());
+    if (json != "")
+        ElseResults.Create(json, args.Any(a => a.Equals("times", StringComparison.CurrentCulture)));
+
+    Console.WriteLine();
+    Console.WriteLine($"Source: {args.First()}");
+    return;
+}
+
 new AdventOfCode10.Aoc2025.Day02().Run();
